ea60405bc5064757fd6e17521ea583f0c8a7ed32
Djo-75 <j.pecquet@live.fr>
2023-03-02 11:02:06 +0100

    script 3

--- script3.js
+++ script3.js
@@ -1,57 +1,17 @@
+
+
+let str = "*";
+let dot = " ";
+
 nombre=parseInt(prompt("Combien veux-tu d'étages ?"));
 
-for(var count_star = 1; count_star <= nombre; count_star++)
+
+for(var count = 1; count <= nombre; count++)
 
 {
-  let str = "*";
-  let multiStr = str.repeat(count_star);
+  let multistar = str.repeat(count)
 
-  console.log(multiStr);
+  let multispace = dot.repeat(nombre - count);
+  console.log(multispace + multistar)
 }
 
-for(var count_dot = nombre; count_dot > 0; count_dot--)
-
-{
-  let dot = "*";
-  let multidot = dot.repeat(count_dot);
-
-  console.log(multidot);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function pyramid(n) {
-
- 
-
-//   for(let i=1; i<= n; i++){
-
-//     let str = ' '.repeat(n-i);
-
-//     let str2 = '*'. repeat(i*2 -1)
-
- 
-
-//     console.log(str + str2 + str);
-
-//   }
-
-// }
-
-// pyramid(5);
