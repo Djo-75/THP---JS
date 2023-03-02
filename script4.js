a0e302edd9bbd7f63df4efd7d8039794004216de
Djo-75 <j.pecquet@live.fr>
2023-03-02 12:07:20 +0100

    script 4

--- script4.js
+++ script4.js
@@ -19,4 +19,8 @@ const entrepreneurs = [
     { first: 'Peter', last: 'Thiel', year: 1967 }
   ];
 
-  
+const between = entrepreneurs.filter(element => {
+return element.year > 1970 && element.year < 1980;
+})
+
+  console.log(between)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+    { first: 'Travis', last: 'Kalanick', year: 1976 },
+    { first: 'Marc', last: 'Andreessen', year: 1971 },
+    { first: 'Peter', last: 'Thiel', year: 1967 }
+  ];
+
+const between = entrepreneurs.filter(element => {
+return element.year > 1970 && element.year < 1980;
+})
